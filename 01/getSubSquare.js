ef03e3cc14236f71a7016985fdd9785591e79d24
AydaFox <burrhe.y02@gmail.com>
2024-04-17 11:49:46 +0100

    adds 4 new katas with instructions to practice

---
+++ 01/getSubSquare.js
@@ -0,0 +1,48 @@
+/*
+Given a square matrix (two-dimensional array) and a 
+xStartIndex and a yStartIndex, you must return a 3 * 3 
+subsquare from the array.
+
+For example, in the following matrix, if asked for the subsquare 
+with start indexes of 0, 0, you would extract a two-dimensional 
+matrix beginning from the top left-hand corner of the matrix:
+
+const matrix = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 4, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+getSubSquare(matrix, 0, 0);
+
+// --> [ [5, 3, 4], [6, 7, 2], [1, 9, 8] ]
+Here is another example:
+
+const matrix = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 4, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+getSubSquare(matrix, 2, 5);
+
+// --> [ [3, 9, 2], [1, 5, 3], [7, 4, 1] ]
+Testing Notes:
+The matrix will always be square and will always have 
+dimensions of at least 3 * 3.
+If yStartIndex or xStartIndex is too high and a complete 3 * 3 
+subsquare cannot be extracted, the function should return a matrix 
+with null values in the overflowed positions.
+*/
---
+++ 01/simplifyDirections.js
@@ -0,0 +1,18 @@
+/*
+The challenge is to implement a function that takes an array of 
+directions and simplifies them. Directions can be simplified if 
+any two directions cause the person to end up in the same place. 
+For example, the directions ["NORTH", "SOUTH"] cancel one another 
+out and the function should return an empty array of no directions [].
+
+
+Examples
+Here are some examples to help you build out your test suite:
+
+simplifyDirections(["NORTH", "SOUTH", "WEST"])
+    --> ["WEST"]
+simplifyDirections(["NORTH", "WEST", "SOUTH", "WEST", "EAST"])
+    --> ["WEST"]
+simplifyDirections(["NORTH", "EAST", "SOUTH", "WEST", "NORTH", "NORTH"])
+    --> ["NORTH", "NORTH"]
+*/
